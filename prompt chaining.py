b7268f2d21b5cb05e0bfdaf6fe0c6c735ad25435
Jamshed Ali <116933185+JamshedAli18@users.noreply.github.com>
2026-03-27 22:30:12 +0500

    Add prompt chaining functionality with ChatGroq
    Implement a prompt chaining system using ChatGroq and StateGraph.

---
+++ prompt chaining.py
@@ -0,0 +1,46 @@
+import os 
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langgraph.graph import START,END,StateGraph
+from typing import TypedDict,Annotated
+
+load_dotenv()
+
+os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+
+groq_model = ChatGroq(model="llama-3.1-8b-instant")
+
+class PromptChainState(TypedDict):
+    topic : str
+    outline: str
+    content: str
+
+def generate_outline(state: PromptChainState) -> PromptChainState:
+    topic = state["topic"]
+    outline = groq_model.invoke(f"Generate a single outline from the topic: {topic}").content
+    
+    return state | {"outline": outline}
+
+def generate_content(state: PromptChainState) -> PromptChainState:
+    topic = state["topic"]
+    outline = state["outline"]
+    content = groq_model.invoke(f"Generate content for blog on the topic: {topic} with outline: {outline}").content
+    
+    return state | {"content": content}
+
+
+graph = StateGraph(PromptChainState)
+
+graph.add_node("generate_outline",generate_outline)
+graph.add_node("generate_content",generate_content)
+
+graph.add_edge(START,"generate_outline")
+graph.add_edge("generate_outline","generate_content")
+graph.add_edge("generate_content",END)
+
+graph = graph.compile()
+
+initial_state = {"topic": "AI"}
+
+for s in graph.stream(initial_state):
+    print(s)
